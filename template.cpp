2003f75147c9d870848b06e9b731b94281ba274f
sykwer <sykwer@gmail.com>
2018-11-28 21:43:19 +0900

    Add template

---
+++ template.cpp
@@ -0,0 +1,47 @@
+/* ---------- STL Libraries ---------- */
+// IO library
+#include <cstdio>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+
+// algorithm library
+#include <algorithm>
+#include <cmath>
+#include <numeric>
+#include <random>
+
+// container library
+#include <array>
+#include <bitset>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <string>
+#include <tuple>
+#include <vector>
+
+/* ---------- Namespace ---------- */
+using namespace std;
+
+/* ---------- Type ---------- */
+using ll = long long;
+#define int ll
+#define P pair<ll, ll>
+
+/* ---------- Constants  */
+const ll MOD = 1e9 + 7;
+// const int INF = 1 << 25;
+// const ll INF = 1LL << 50;
+// const double PI = acos(-1);
+// const double EPS = 1e-10;
+// mt19937 mert(LL(time(0)));
+
+/* v-v-v-v-v-v-v-v-v Main Part v-v-v-v-v-v-v-v-v */
+
+int main() {
+    return 0;
+}
