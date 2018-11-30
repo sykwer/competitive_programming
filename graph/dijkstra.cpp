2dd51392a454e44fe5781f64c373ee7481432384
sykwer <sykwer@gmail.com>
2018-11-30 10:51:12 +0900

    Wrap dijkstra into undirected graph class2

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -4,53 +4,59 @@
 using namespace std;
 
 const int INF = 1 << 25;
-const int MAX_V = 10000;
-
-struct edge {
-    int to;
-    int cost;
-};
-
 typedef pair<int, int> P; // min distance, vertex index
 
-int V;
-vector<edge> G[MAX_V];
-int d[MAX_V];
+class UndirectedGraph {
+    struct edge {
+        int to;
+        int cost;
+    };
 
-// O(E * logV)
-void dijkstra(int s) {
-    priority_queue<P, vector<P>, greater<>> que;
-    fill(d, d + V, INF);
-    d[s] = 0;
-    que.push(P(0, s));
+public:
+    int V;
+    vector<vector<edge>> G;
 
-    while (!que.empty()) {
-        P p = que.top(); que.pop();
-        int v = p.second;
-        if (d[v] < p.first) continue;
+    UndirectedGraph(int v) : G(v), V(v) {}
 
-        for (int i = 0; i < G[v].size(); i++) {
-            edge e = G[v][i];
-            if (d[e.to] > d[v] + e.cost) {
-                d[e.to] = d[v] + e.cost;
-                que.push(P(d[e.to], e.to));
+    void add_edge(int u, int v, int cost) {
+        G[u].push_back({v, cost});
+        G[v].push_back({u, cost});
+    }
+
+    // O(E * logV)
+    vector<int> dijkstra(int s) {
+        vector<int> d(V, INF);
+        d[s] = 0;
+
+        priority_queue<P, vector<P>, greater<>> que;
+        que.push({0, s});
+
+        while (!que.empty()) {
+            P  p = que.top(); que.pop();
+            int v = p.second;
+            if (d[v] < p.first) continue;
+
+            for (edge e : G[v]) {
+                if (d[e.to] > d[v] + e.cost) {
+                    d[e.to] = d[v] + e.cost;
+                    que.push({d[e.to], e.to});
+                }
             }
         }
+
+        return move(d);
     }
-}
+};
 
 int main() {
-    V = 3;
-    G[0].push_back({1, 1});
-    G[0].push_back({2, 4});
-    G[1].push_back({0, 1});
-    G[1].push_back({2, 2});
-    G[2].push_back({0, 4});
-    G[2].push_back({1, 2});
+    UndirectedGraph ug = UndirectedGraph(3);
+    ug.add_edge(0, 1, 1);
+    ug.add_edge(0, 2, 4);
+    ug.add_edge(1, 2, 2);
 
-    dijkstra(0); // shortest paths from node-0
+    vector<int> d = ug.dijkstra(0);
 
-    for (int i = 0; i < V; i++) {
+    for (int i = 0; i < ug.V; i++) {
         cout << "cost 0->" << i << " : " << d[i] << endl;
     }
 
