bb2ba38e20a6dd8a422a32bed4831a2cac39ea1e
sykwer <sykwer@gmail.com>
2018-11-30 11:22:00 +0900

    Wrap bellman_form into DirectedGraph class

--- graph/bellman_ford.cpp
+++ graph/bellman_ford.cpp
@@ -1,69 +1,75 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
 const int INF = 1 << 25;
 
-const int MAX_E = 10000;
-const int MAX_V = 1000;
-int V;
-int E;
+class DirectedGraph {
+    struct edge {
+        int from;
+        int to;
+        int cost;
+    };
 
-struct edge {
-    int from;
-    int to;
-    int cost;
-};
+    int edge_num = 0;
 
-edge es[MAX_E];
-int d[MAX_V]; // shortest path
+public:
+    int V;
+    int E;
+    vector<edge> edges;
 
-// O(V * E)
-void bellman_ford(int s) {
-    fill(d, d + V, INF);
-    d[s] = 0;
+    DirectedGraph(int v, int e) : V(v), edges(e), E(e) {};
 
-    while (true) {
-        bool update = false;
+    void add_edge(int from, int to, int cost) {
+        edges[edge_num++] = {from, to, cost};
+    }
+
+    // O(E * V)
+    vector<int> bellman_ford(int s) {
+        vector<int> d(V, INF);
+        d[s] = 0;
+
+        while (true) {
+            bool update = false;
+            for (int i = 0; i < E; i++) {
+                edge e = edges[i];
+                if (d[e.from] != INF && d[e.to] > d[e.from] + e.cost) {
+                    d[e.to] = d[e.from] + e.cost;
+                    update = true;
+                }
+            }
+
+            if (!update) break;
+        }
+
+        return move(d);
+    }
+
+    bool find_negative_loop() {
+        vector<int> d(V, 0);
 
         for (int i = 0; i < E; i++) {
-            edge e = es[i];
-            if (d[e.from] != INF && d[e.to] > d[e.from] + e.cost) {
+            edge e = edges[i];
+            if (d[e.to] > d[e.from] + e.cost) {
                 d[e.to] = d[e.from] + e.cost;
-                update = true;
+
+                if (i == V - 1) return true;
             }
         }
 
-        if (!update) break;
+        return false;
     }
-}
-
-// if true, there is negative loop
-bool find_negative_loop() {
-    memset(d, 0, sizeof(d));
-
-    for (int i = 0; i < V; i++) {
-        edge e = es[i];
-        if (d[e.to] > d[e.from] + e.cost) {
-            d[e.to] = d[e.from] + e.cost;
-
-            if (i == V - 1) return true;
-        }
-    }
-
-    return false;
-}
+};
 
 int main() {
-    V = 3;
-    E = 3;
-    es[0] = {0, 1, 1};
-    es[1] = {0, 2, 4};
-    es[2] = {1, 2, 2};
+    DirectedGraph dg = DirectedGraph(3, 3);
+    dg.add_edge(0, 1, 1);
+    dg.add_edge(0, 2, 4);
+    dg.add_edge(1, 2, 2);
+    vector<int> d = dg.bellman_ford(0);
 
-    bellman_ford(0); // shortest paths from node-0
-
-    for (int i = 0; i < V; i++) {
+    for (int i = 0; i < dg.V; i++) {
         cout << "cost 0->" << i << " : " << d[i] << endl;
     }
 
