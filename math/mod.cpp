60fc1757f02d947ae1ba7a7f1c14ffcf7e8116f5
sykwer <sykwer@gmail.com>
2018-12-01 15:15:19 +0900

    Add mod

---
+++ math/mod.cpp
@@ -0,0 +1,14 @@
+// O(logn)
+int mod_pow(int x, int n, int mod) {
+    int ret = 1;
+    while (n > 0) {
+        if (n & 1) ret = ret * x % mod;
+        x = x * x % mod;
+        n >>= 1;
+    }
+
+    return ret;
+}
+
+signed main() {
+}
