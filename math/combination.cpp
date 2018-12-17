ec5c8f8901641c3203fd69334c3e2d3019418473
sykwer <sykwer@gmail.com>
2018-12-17 15:28:57 +0900

    Add fermat's little theorem combination

--- math/combination.cpp
+++ math/combination.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+const long long MOD = 1e9 + 7;
+#define int long long
+
 // O(N^2)
 vector<vector<int>> set_pascal(int N) {
     vector<vector<int>> c(N + 1, vector<int>(N + 1, 0));
@@ -33,6 +36,44 @@ vector<vector<int>> set_pascal(int N, int mod) {
     return move(c);
 }
 
+// O(n)
+class Combination {
+    int powmod(int a, int p) {
+        int ans = 1;
+        int mul = a;
+
+        for (; p > 0; p >>= 1, mul = (mul * mul) % mod) {
+            if ((p & 1) == 1) ans = (ans * mul) % mod;
+        }
+
+        return ans;
+    }
+public:
+    int N;
+    int mod;
+    vector<int> fact;
+    vector<int> revFact;
+
+    Combination(int n, int m) : N(n), mod(m), fact(n+1), revFact(n+1) {
+        fact[0] = 1;
+        for (int i = 1; i <= N; i++) {
+            fact[i] = fact[i-1] * i;
+            fact[i] %= mod;
+        }
+
+        // Fermat's little theorem
+        revFact[N] = powmod(fact[N], mod - 2);
+
+        for (int i = N-1; i >= 0; i--) {
+            revFact[i] = revFact[i+1] * (i+1);
+            revFact[i] %= mod;
+        }
+    }
+
+    int get(int a, int b) {
+        return (((fact[a] * revFact[b]) % mod) * revFact[a-b]) % mod;
+    }
+};
 
 signed main() {
 }
