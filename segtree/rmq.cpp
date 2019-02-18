74ef02663c6a86a20aae8fa4937f269c983cb067
sykwer <sykwer@gmail.com>
2019-02-18 16:32:10 +0900

    Update RMQ

--- segtree/rmq.cpp
+++ segtree/rmq.cpp
@@ -1,84 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+
 using namespace std;
 
 const int INF = INT_MAX;
 
 // Range Minimum Query
-struct RMQ {
+struct SegmentTree {
+private:
     int n;
-    vector<int> dat;
+    vector<int> node;
 
-    RMQ() {}
-
-    RMQ(int n_) {
-        init(n_);
-    }
-
-    RMQ(int n_, int* a) {
-        init(n_);
-        build(n_, a);
-    }
-
-    void init(int n_) {
+public:
+    SegmentTree(vector<int> v) {
+        int sz = v.size();
         n = 1;
-        while (n < n_) n *= 2;
-        dat.clear();
-        dat.resize(2 * n - 1, INF);
+        while (n < sz) n *= 2;
+        node.resize(2 * n - 1, INF);
+        for (int i = 0; i < sz; i++) node[i + n - 1] = v[i];
+        for (int i = n - 2; i >= 0; i--) node[i] = min(node[2 * i + 1], node[2 * i + 2]);
     }
 
-    void build(int n_, int* a) {
-        for (int i = 0; i < n_; i++) dat[i + n - 1] = a[i];
-        for (int i = n - 2; i >= 0; i--) dat[i] = min(dat[i * 2 + 1], dat[i * 2 + 2]);
-    }
-
-    void update(int k, int a) {
-        k += n - 1;
-        dat[k] = a;
-
-        while (k > 0) {
-            k = (k - 1) / 2;
-            dat[k] = min(dat[k * 2 + 1], dat[k * 2 + 2]);
+    void update(int x, int val) {
+        x += (n - 1);
+        node[x] = val;
+        while (x > 0) {
+            x = (x - 1) / 2;
+            node[x] = min(node[2 * x + 1], node[2 * x + 2]);
         }
     }
 
-    int query(int a, int b, int k, int l, int r) {
+    int getmin(int a, int b, int k = 0, int l = 0, int r = -1) {
+        if (r < 0) r = n;
         if (r <= a || b <= l) return INF;
-        if (a <= l && r <= b) return dat[k];
+        if (a <= l && r <= b) return node[k];
 
-        int vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
-        int vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
+        int vl = getmin(a, b, 2 * k + 1, l, (l + r) / 2);
+        int vr = getmin(a, b, 2 * k + 2, (l + r) / 2, r);
         return min(vl, vr);
-    }
-
-    int query(int a, int b) {
-        return query(a, b, 0, 0, n);
     }
 };
 
 signed main() {
-    int n, q;
-    cin >> n >> q;
-
-    int initial[n];
-    for (int i = 0; i < n; i++) initial[i] = INF;
-
-    RMQ rmq = RMQ(n, initial);
-
-    const int UPDATE = 0;
-    const int FIND = 1;
-
-    for (int i = 0; i < q; i++) {
+    int N, Q;
+    cin >> N >> Q;
+    SegmentTree seg(vector<int>(N, INF));
+    for (int i = 0; i < Q; i++) {
         int c, x, y;
         cin >> c >> x >> y;
-
-        if (c == UPDATE) {
-            rmq.update(x, y);
-        } else if (c == FIND) {
-            cout << rmq.query(x, y + 1) << endl;
-        }
+        if (c == 0) seg.update(x, y);
+        else cout << seg.getmin(x, y + 1) << endl;
     }
-
-    return 0;
 }
