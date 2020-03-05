798026f224b3840a71ad0ca966c298ae452b243f
sykwer <sykwer@gmail.com>
2020-03-05 09:49:23 +0900

    Change tempate

--- template.cpp
+++ template.cpp
@@ -38,12 +38,18 @@ using namespace std;
 /* ---------- Type ---------- */
 using ll = long long;
 #define int ll
+template <class T> using MaxHeap = priority_queue<T>;
+template <class T> using MinHeap = priority_queue<T, vector<T>, greater<T>>;
 #define P pair<ll, ll>
 
 /* ---------- Constants  */
 const double PI = 3.141592653589793238462643383279;
 const ll MOD = 1e9 + 7;
 const int INF = 1LL << 55;
+
+/* ---------- Functions */
+template<class T> bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template<class T> bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
 
 /* v-v-v-v-v-v-v-v-v Main Part v-v-v-v-v-v-v-v-v */
 signed main() {
