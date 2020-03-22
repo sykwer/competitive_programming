bdc9e0dfe8c4b7440b49b03a4cd9e7a4cbdc2050
sykwer <sykwer@gmail.com>
2020-03-22 15:46:17 +0900

    Rename

--- segtree/segtree_point_update_range_query.cpp
+++ segtree/segtree_point_update_range_query.cpp
@@ -9,7 +9,7 @@ template <class T> class SegTree {
     vector<T> data;
     T ide; // Identity element (Initial value)
     function<T(T, T)> operation; // Function for range query
-    function<T(T, T)> update; // Function for point update
+    function<T(T, T)> updater; // Function for point update
 
     T _query(int a, int b, int k, int l, int r) {
         if (r <= a || b <= l) return ide;
@@ -26,17 +26,17 @@ public:
     // int n:                       Size of array
     // T ide:                       Identity element
     // function<T(T, T)> operation: Function to merge elements
-    // function<T(T, T)> update:    (old data[i], some value x) -> new data[i]
+    // function<T(T, T)> updater:    (old data[i], some value x) -> new data[i]
     SegTree(size_t n, T ide, function<T(T, T)> operation,
-            function<T(T, T)> update) : ide(ide), operation(operation), update(update) {
+            function<T(T, T)> updater) : ide(ide), operation(operation), updater(updater) {
         N = 1;
         while (N < n) N *= 2;
         data.resize(2 * N - 1, ide);
     }
 
-    void change(int i, T x) {
+    void update(int i, T x) {
         i += N - 1;
-        data[i] = update(data[i], x);
+        data[i] = updater(data[i], x);
         while (i > 0) {
             i = (i - 1) / 2;
             data[i] = operation(data[i * 2 + 1], data[i * 2 + 2]);
@@ -59,13 +59,13 @@ int main() {
 
     ll ide = (1LL << 31) - 1;
     auto operation = [](ll left, ll right) -> ll { return min(left, right); };
-    auto change = [](ll old, ll x) -> ll { return x; };
-    auto seg = new SegTree<ll>(N, ide, operation, change);
+    auto updater = [](ll old, ll x) -> ll { return x; };
+    auto seg = new SegTree<ll>(N, ide, operation, updater);
 
     for (int q = 0; q < Q; q++) {
         int c, x, y;
         cin >> c >> x >> y;
-        if (!c) seg->change(x, y);
+        if (!c) seg->update(x, y);
         else cout << seg->query(x, y+1) << endl;
     }
 }
